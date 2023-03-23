37b45d0623e689a470ae4a383b1d1bc790e2c10b
Theresa1903GitHub <115719578+Theresa1903GitHub@users.noreply.github.com>
2023-03-23 12:16:07 +0100

    Update

---
+++ A01_RandomPoem/RandomPoem.js
@@ -0,0 +1,21 @@
+"use strict";
+var Poem;
+(function (Poem) {
+    let subjects = ["Harry", "Hermine", "Ron", "Hagrid", "Snape", "Dumbledore"];
+    let verbs = ["braut", "liebt", "studiert", "hasst", "zaubert", "zerstört"];
+    let objects = ["Zaubertränke", "den Grimm", "Lupin", "Hogwarts", "die Karte des Rumtreibers", "Dementoren"];
+    //console.log(subjects);
+    console.log(verbs);
+    console.log(objects);
+    for (let x = subjects.length; x >= 1; x--) {
+        console.log(x);
+    }
+    function getVerse(_subject, _verb, _object) {
+        //console.log(_x);			
+        return _subject + " " + _verb + " " + _object;
+        console.log(_subject + " " + _verb + " " + _object);
+    }
+    ;
+    getVerse("Alo", "mo", "loa");
+})(Poem || (Poem = {}));
+//# sourceMappingURL=RandomPoem.js.map
---
+++ A01_RandomPoem/RandomPoem.ts
@@ -0,0 +1,24 @@
+namespace Poem {
+
+let subjects : string [] = ["Harry", "Hermine", "Ron", "Hagrid", "Snape", "Dumbledore"];
+let verbs : string [] = ["braut", "liebt", "studiert", "hasst", "zaubert", "zerstört"];
+let objects : string [] = ["Zaubertränke", "den Grimm", "Lupin", "Hogwarts", "die Karte des Rumtreibers", "Dementoren"]
+
+//console.log(subjects);
+console.log(verbs);
+console.log(objects);
+
+for (let x:number=subjects.length; x>=1; x--){
+    console.log(x);
+}
+
+function getVerse (_subject:string, _verb:string, _object:string) : string {
+    //console.log(_x);			
+    return _subject + " " + _verb + " " + _object
+    console.log(_subject + " " + _verb + " " + _object);
+};
+
+getVerse ("Alo", "mo", "loa");
+
+}
+
