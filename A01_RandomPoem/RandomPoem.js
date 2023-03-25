71c4b48b97bae719083a246c48bbbce9f4d688d4
Theresa1903GitHub <115719578+Theresa1903GitHub@users.noreply.github.com>
2023-03-25 14:18:06 +0100

    Update RandomPoem

--- A01_RandomPoem/RandomPoem.js
+++ A01_RandomPoem/RandomPoem.js
@@ -4,18 +4,33 @@ var Poem;
     let subjects = ["Harry", "Hermine", "Ron", "Hagrid", "Snape", "Dumbledore"];
     let verbs = ["braut", "liebt", "studiert", "hasst", "zaubert", "zerstört"];
     let objects = ["Zaubertränke", "den Grimm", "Lupin", "Hogwarts", "die Karte des Rumtreibers", "Dementoren"];
-    //console.log(subjects);
-    console.log(verbs);
-    console.log(objects);
+    // console.log(subjects);
+    // console.log(verbs);
+    // console.log(objects);
+    // let array1 :string [] = [];
+    // let array2 :string [] = [];
+    // let array3 :string [] = [];
+    // let verse : string = "";
+    // console.log(randomSubject);
+    // console.log(randomVerb);
+    // console.log(randomObject);
+    // subjects.splice(randomSubject,1);
+    // verbs.splice(randomVerb,1);
+    // objects.splice(randomObject,1);
     for (let x = subjects.length; x >= 1; x--) {
-        console.log(x);
+        getVerse(subjects, verbs, objects);
     }
     function getVerse(_subject, _verb, _object) {
-        //console.log(_x);			
-        //return _subject + " " + _verb + " " + _object;
-        console.log(_subject + " " + _verb + " " + _object);
+        let randomSubject = Math.floor(Math.random() * subjects.length);
+        let randomObject = Math.floor(Math.random() * objects.length);
+        let randomVerb = Math.floor(Math.random() * verbs.length);
+        let verse = subjects[randomSubject] + " " + verbs[randomVerb] + " " + objects[randomObject];
+        _subject.splice(randomSubject, 1);
+        _verb.splice(randomVerb, 1);
+        _object.splice(randomObject, 1);
+        console.log(verse);
+        return verse;
     }
     ;
-    getVerse("Alo", "mo", "loa");
 })(Poem || (Poem = {}));
 //# sourceMappingURL=RandomPoem.js.map
--- A01_RandomPoem/RandomPoem.ts
+++ A01_RandomPoem/RandomPoem.ts
@@ -1,24 +1,46 @@
 namespace Poem {
 
-let subjects : string [] = ["Harry", "Hermine", "Ron", "Hagrid", "Snape", "Dumbledore"];
-let verbs : string [] = ["braut", "liebt", "studiert", "hasst", "zaubert", "zerstört"];
-let objects : string [] = ["Zaubertränke", "den Grimm", "Lupin", "Hogwarts", "die Karte des Rumtreibers", "Dementoren"]
+    let subjects: string[] = ["Harry", "Hermine", "Ron", "Hagrid", "Snape", "Dumbledore"];
+    let verbs: string[] = ["braut", "liebt", "studiert", "hasst", "zaubert", "zerstört"];
+    let objects: string[] = ["Zaubertränke", "den Grimm", "Lupin", "Hogwarts", "die Karte des Rumtreibers", "Dementoren"]
+    // console.log(subjects);
+    // console.log(verbs);
+    // console.log(objects);
 
-//console.log(subjects);
-console.log(verbs);
-console.log(objects);
+    // let array1 :string [] = [];
+    // let array2 :string [] = [];
+    // let array3 :string [] = [];
 
-for (let x:number=subjects.length; x>=1; x--){
-    console.log(x);
-}
+    // let verse : string = "";
 
-function getVerse (_subject:string, _verb:string, _object:string) : void {
-    //console.log(_x);			
-    //return _subject + " " + _verb + " " + _object;
-    console.log(_subject + " " + _verb + " " + _object);
-};
+    // console.log(randomSubject);
+    // console.log(randomVerb);
+    // console.log(randomObject);
 
-getVerse ("Alo", "mo", "loa");
+    // subjects.splice(randomSubject,1);
+    // verbs.splice(randomVerb,1);
+    // objects.splice(randomObject,1);
+
+
+    for (let x: number = subjects.length; x >= 1; x--) {
+        getVerse(subjects, verbs, objects)
+    }
+
+    function getVerse(_subject: string[], _verb: string[], _object: string[]): string {
+        let randomSubject: number = Math.floor(Math.random() * subjects.length);
+        let randomObject: number = Math.floor(Math.random() * objects.length);
+        let randomVerb: number = Math.floor(Math.random() * verbs.length);
+        let verse: string = subjects[randomSubject] + " " + verbs[randomVerb] + " " + objects[randomObject];
+
+        _subject.splice(randomSubject, 1);
+        _verb.splice(randomVerb, 1);
+        _object.splice(randomObject, 1);
+
+        console.log(verse);
+        return verse;
+
+    };
+
 
 }
 
